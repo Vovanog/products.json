644233d8977a2ed61146cde17375a0dd4fd7455f
«vovanog» <zabotinskijvova@gmail.com>
2024-12-22 14:33:33 +0000

    sffs

--- main.js
+++ main.js
@@ -6,6 +6,15 @@ function getProductHtml(product) {
       <h5 class="card-title">${product.title}</h5>
       <p class="card-text">${product.description}</p>
       <a href="#" class="btn btn-primary">${product.price}$</a>
+
+<button class="button cart-btn"  data-product='${JSON.stringify(product)}'>
+ <svg viewBox="0 0 16 16" class="bi bi-cart-check" height="24" width="24" xmlns="http://www.w3.org/2000/svg" fill="#fff">
+  <path d="M11.354 6.354a.5.5 0 0 0-.708-.708L8 8.293 6.854 7.146a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3z"></path>
+  <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"></path>
+</svg>
+  <p class="text">Buy Now</p>
+</button>
+
     </div>
   </div>
   `
@@ -24,9 +33,71 @@ getProducts().then(function (products) {
       productsContainer.innerHTML += getProductHtml(product)
     })
   }
+  let buyButtons = document.querySelectorAll('.cart-btn')
+
+if(buyButtons.length > 0) {
+    buyButtons.forEach(function(button) {
+        button.addEventListener('click', addToCart)
+    })
+}
 })
 const themeToggle = document.getElementById('themeToggle');
 const body = document.querySelector("body")
 themeToggle.addEventListener('click', () => {
   document.body.classList.toggle('dark-theme');
 });
+
+
+
+class Cart {
+  constructor() {
+      this.items = {}
+      this.total = 0
+  }
+
+  addItem(item) {
+      if(this.items[item.title]) {
+          this.items[item.title].quantity += 1
+      } else {
+          this.items[item.title] = item
+          this.items[item.title].quantity = 1
+      }
+      this.saveCartToCookies()
+  }
+
+  saveCartToCookies() {
+      let cartJSON = JSON.stringify(this.items)
+      document.cookie = `cart=${cartJSON}; max-age=${60 * 60 * 24 * 7}; path=/`
+
+      console.log(document.cookie)
+  }
+
+  loadCartToCookies() {
+      let cartCookies = getCookieValue('cart');
+
+      if (cartCookies && cartCookies !== '') {
+          this.items = JSON.parse(cartCookies)
+      }
+  }
+}
+
+let cart = new Cart();
+
+
+function addToCart(event) {
+  const productData = event.target.getAttribute('data-product')
+  const product = JSON.parse(productData)
+
+  cart.addItem(product)
+}
+
+function getCookieValue(cookieName) {
+  const cookies = document.cookie.split('; ');
+  for (const cookie of cookies) {
+      const [name, value] = cookie.split('=');
+      if (name === cookieName) {
+          return decodeURIComponent(value);
+      }
+  }
+  return null;
+}

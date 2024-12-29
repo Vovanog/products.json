164b65c525569bb4286a2b564d32332d8138eac9
«vovanog» <zabotinskijvova@gmail.com>
2024-12-29 14:00:23 +0000

    іфі

--- main.js
+++ main.js
@@ -8,11 +8,7 @@ function getProductHtml(product) {
       <a href="#" class="btn btn-primary">${product.price}$</a>
 
 <button class="button cart-btn"  data-product='${JSON.stringify(product)}'>
- <svg viewBox="0 0 16 16" class="bi bi-cart-check" height="24" width="24" xmlns="http://www.w3.org/2000/svg" fill="#fff">
-  <path d="M11.354 6.354a.5.5 0 0 0-.708-.708L8 8.293 6.854 7.146a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3z"></path>
-  <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"></path>
-</svg>
-  <p class="text">Buy Now</p>
+ b
 </button>
 
     </div>
@@ -53,6 +49,7 @@ class Cart {
   constructor() {
       this.items = {}
       this.total = 0
+      this.loadCartToCookies()
   }
 
   addItem(item) {
@@ -87,7 +84,7 @@ let cart = new Cart();
 function addToCart(event) {
   const productData = event.target.getAttribute('data-product')
   const product = JSON.parse(productData)
-
+console.log(product,productData)
   cart.addItem(product)
 }
 
@@ -101,3 +98,29 @@ function getCookieValue(cookieName) {
   }
   return null;
 }
+function getCartProductHtml(item) {
+  return `
+      <div style="border: 1px solid black">
+          <p>${item.title}</p>
+          <p>${item.price}</p>
+          <p>${item.quantity}</p>
+      </div>
+`
+}
+
+function showCart() {
+  const cartContainer = document.querySelector('.cart-container');
+  if (!cartContainer) {
+      console.error('Cart container not found in DOM.');
+      return;
+  }
+  cartContainer.innerHTML = '';
+  for (let key in cart.items) {
+      cartContainer.innerHTML += getCartProductHtml(cart.items[key]);
+  }
+
+  cart.loadCartToCookies(); }
+
+  window.addEventListener('DOMContentLoaded', () => {
+    showCart();
+});
